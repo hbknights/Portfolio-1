48a5d00b90bf6075d95e2a911c85d3cb7477ed58
hbknights <purpleelephants@knights.ucf.edu>
2021-03-03 18:59:42 -0500

    First Commit

--- portfolio1/src/components/Food/index.js
+++ portfolio1/src/components/Food/index.js
@@ -13,19 +13,37 @@ export default class FoodScreen extends React.Component {
     render(){
       
         let objects = [
-            { objType: "food", name: " Hard Boiled Eggs ", value: 0 },
+            { objType: "food", name: "Almonds", value: 2 },
+            { objType: "food", name: "Animal Crackers", value: 3 },
+            { objType: "food", name: "Asparagus", value: 0 },
+            { objType: "food", name: "Bacon", value: 3 },
+            { objType: "food", name: "Bagel", value: 3},
+            { objType: "food", name: "Banana", value: 2 },
+            { objType: "food", name: "Bread", value: 2 },
+            { objType: "food", name: "Chocolate", value: 3 },
+            { objType: "food", name: "Coca-Cola", value: 3 },
             { objType: "food", name: "Cucumber", value: 0 },
-            { objType: "food", name: "Lucky Charms", value: 25 },
-            { objType: "food", name: "Chocolate Pudding", value: 45 },
-            { objType: "food", name: "Donuts", value: 50 },
-            { objType: "food", name: "Tomatoes", value: 0 },
+            { objType: "food", name: "Donuts", value: 5 },
+            { objType: "food", name: "Hamburger", value: 6 },
+            { objType: "food", name: "Hard Boiled Eggs ", value: 0 },
+            { objType: "food", name: "Lettuce", value: 0 },
+            { objType: "food", name: "Lucky Charms", value: 5 },
+            { objType: "food", name: "Milk", value: 4 },
+            { objType: "food", name: "Pizza", value: 6 },
+            { objType: "food", name: "Potato", value: 3 },             
+            { objType: "food", name: "Rice", value: 4 },
+            { objType: "food", name: "Sprite", value: 3 },
+            { objType: "food", name: "Watermelon", value: 1 },
+            { objType: "food", name: "Yogurt", value: 4 },
           ]
         return(
             <>
             <div style={{backgroundColor: "#90ee90"}}>
               <center>
         <h2>Food Points</h2>
-        <p>You  have 300 points per day</p>
+        <p>You  have 150 points per day</p>
+        Calories: {this.state.value}
+              <br></br>
         <ul style={{ "listStyleType": "none" }}>
                 {objects.map((obj, index) =>
                   <li key={index}>
@@ -41,8 +59,7 @@ export default class FoodScreen extends React.Component {
                 )}
 
               </ul>
-              Calories: {this.state.value}
-              <br></br>
+              
               </center>
               </div>
         </>
